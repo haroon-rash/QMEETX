49da3b70a4bf77fcd96f2a279220721a62782662
Muhammad Haroon Ur Rasheed <haroonurrasheed1212@gmail.com>
2026-01-13 01:40:47 +0500

    commit  changes

---
+++ Parent-Proto/src/test/java/com/qmeetx/parentproto/ParentProtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qmeetx.parentproto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentProtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
